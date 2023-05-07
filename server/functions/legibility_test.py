892139fab7adbad4ca2361d929429cf558e3405c
Rijrya <richard.yunqi.zhu@gmail.com>
2023-05-06 20:35:56 -0400

    Fixed bug

--- server/functions/legibility_test.py
+++ server/functions/legibility_test.py
@@ -6,7 +6,7 @@ def is_legible(text):
     co = cohere.Client(COHERE_SECRET_KEY) 
     response = co.classify(
     model='large',
-    inputs=["text"],
+    inputs=[text],
     examples=[Example("jjfiasfijss", "Gibberish"),
                 Example("Hello, how are you", "Legible"),
                 Example("a. _-F. , =. _ S _. eee eo PReESCA!. OLY. (tel el. ye VA LA. _. ms. inet. 8CEro - fi pa rola, L-valine (47 rN ,\"}\nLOG a. _-F. , =. _ S _. eee eo PReESCA!. OLY. (tel el. ye VA LA. _. ms. inet. 8CEro - fi pa rola, L-valine (47 rN ,", "Gibberish"),
@@ -17,6 +17,9 @@ def is_legible(text):
                 Example("US es. BOBBY RD. 7. 32 33 3 35 36 37 3S 22 Pas = 23 T- 2 me. . animated. tewalees 1 geration: 52 a\"}\nLOG US es. BOBBY RD. 7. 32 33 3 35 36 37 3S 22 Pas = 23 T- 2 me. . animated. tewalees 1 geration: 52 a", "Gibberish"),
                 Example("ieuhruoeirwo 7364", "Gibberish"),
                 Example("mAJOR LEAGUE HACKING. Activate $1 00 in free Azure credits! rhackathon project On the cloud by activating your Azure for Students offer. Seamlessly deploy from GitHub and use services like machine learning, low code tools, and much more!. Build you. hackp.aci microsoft", "Legible"),
-                Example("what no one h as done anything at all richard has like stone tools sam has iron tools people didn't join cause hackathon this weekend i grinded because i am a jobles if you join you will be the 4th person how about it", "Legible"),])
+                Example("what no one h as done anything at all richard has like stone tools sam has iron tools people didn't join cause hackathon this weekend i grinded because i am a jobles if you join you will be the 4th person how about it", "Legible"),
+                Example("Avil 7 7 estoelling outhor of THE GLASS HOTEL Uf 000 STATION ELEVEN", "Gibberish")])
     legibility = response[0].confidence
+    print(legibility)
+    print(response[0].confidence)
     return legibility > 0.75
