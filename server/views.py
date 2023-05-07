3db859a61ed94d663c1dbb538203396e4a4f8a54
Steven <wusteven815@gmail.com>
2023-05-06 23:37:47 -0400

    feat: finish summarize api

--- server/views.py
+++ server/views.py
@@ -3,10 +3,11 @@ from base64 import b64decode
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from PIL import Image
+from PIL import Image as PILImage
 from io import BytesIO
 from time import time
 from hashlib import md5
+from server.functions.advanced_funcs import get_summary
 
 from server.models import Image
 from server.serializers import ImageSerializer
@@ -16,35 +17,47 @@ from server.functions.ocr import pytesseract_read_image
 @api_view(["POST"])
 def read_image(request):
 
+    print("[READ] start read")
+
     try:
         raw_img = b64decode(request.data["image"].replace(";", "/"))
-    except:
+    except Exception as e:
+        print("[READ] failed")
+        print(e)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    image = Image.open(BytesIO(raw_img))
+    print("[READ] convert")
+    image = PILImage.open(BytesIO(raw_img))
     text = pytesseract_read_image(image)
     key = md5(str(time()).encode()).hexdigest() + str(round(time()))
     serializer = ImageSerializer(data=dict(id=key, text=text))
+    print(f"[READ] key: {key}")
 
     if serializer.is_valid():
+        print("[READ] start save")
         serializer.save()
-        print(f"read success: {key}")
+        print("[READ] save finish")
         return Response(dict(id=key, text=text))
 
-    print(f"read fail: {key}")
+    print("[READ] invalid format")
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
 def summarize_image(request, key):
 
+    print(f"[SUMMARIZE] start on {key}")
+
     try:
-        product = Image.objects.get(pk=key)
+        image = Image.objects.get(pk=key)
     except Image.DoesNotExist:
-        print(f"summ fail: {key}")
+        print("[SUMMARIZE] key does not exist")
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    print(f"summ success: {key}")
+    print("[SUMMARIZE] get summary")
+    summary = get_summary(dict(ImageSerializer(image).data)["text"])
+
+    print("[SUMMARIZE] finish")
     return Response({
-        "text": "summarized"
+        "text": summary
     })
